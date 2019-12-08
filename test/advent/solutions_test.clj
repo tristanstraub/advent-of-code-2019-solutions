f161f015a62c9fc07006d105cf56b4dbfd990044
Tristan Straub <tristanstraub@gmail.com>
2019-12-08 18:00:37 +1100

    Uncomment missing tests

--- test/advent/solutions_test.clj
+++ test/advent/solutions_test.clj
@@ -32,45 +32,45 @@
            (let [[noun verb] (solutions/matching-inputs (solutions/computer-memory) 19690720)]
              (+ verb (* noun 100)))))))
 
-;; (deftest day-3-part-1
-;;   (testing "part-1"
-;;     (is (= 6
-;;            (solutions/closest-manhattan-to-center [(solutions/parse-wire "R8,U5,L5,D3")
-;;                                                    (solutions/parse-wire "U7,R6,D4,L4")])))
+(deftest day-3-part-1
+  (testing "part-1"
+    (is (= 6
+           (solutions/closest-manhattan-to-center [(solutions/parse-wire "R8,U5,L5,D3")
+                                                   (solutions/parse-wire "U7,R6,D4,L4")])))
 
-;;     (is (= 159
-;;            (solutions/closest-manhattan-to-center [(solutions/parse-wire "R75,D30,R83,U83,L12,D49,R71,U7,L72")
-;;                                                    (solutions/parse-wire "U62,R66,U55,R34,D71,R55,D58,R83")])))
+    (is (= 159
+           (solutions/closest-manhattan-to-center [(solutions/parse-wire "R75,D30,R83,U83,L12,D49,R71,U7,L72")
+                                                   (solutions/parse-wire "U62,R66,U55,R34,D71,R55,D58,R83")])))
 
-;;     (is (= 135
-;;            (solutions/closest-manhattan-to-center (map solutions/parse-wire ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
-;;                                                                              "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]))))
+    (is (= 135
+           (solutions/closest-manhattan-to-center (map solutions/parse-wire ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+                                                                             "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]))))
 
-;;     (is (= 399
-;;            (solutions/closest-manhattan-to-center (solutions/wires)))))
-;;   (testing "part-2"
-;;     (is (= 30
-;;            (solutions/closest-length-to-center (map solutions/parse-wire ["R8,U5,L5,D3" "U7,R6,D4,L4"]))))
-;;     (is (= 610
-;;            (solutions/closest-length-to-center (map solutions/parse-wire ["R75,D30,R83,U83,L12,D49,R71,U7,L72"
-;;                                                                           "U62,R66,U55,R34,D71,R55,D58,R83"]))))
+    (is (= 399
+           (solutions/closest-manhattan-to-center (solutions/wires)))))
+  (testing "part-2"
+    (is (= 30
+           (solutions/closest-length-to-center (map solutions/parse-wire ["R8,U5,L5,D3" "U7,R6,D4,L4"]))))
+    (is (= 610
+           (solutions/closest-length-to-center (map solutions/parse-wire ["R75,D30,R83,U83,L12,D49,R71,U7,L72"
+                                                                          "U62,R66,U55,R34,D71,R55,D58,R83"]))))
 
-;;     (is (= 610
-;;            (solutions/closest-length-to-center (map solutions/parse-wire ["R75,D30,R83,U83,L12,D49,R71,U7,L72"
-;;                                                                           "U62,R66,U55,R34,D71,R55,D58,R83"]))))
+    (is (= 610
+           (solutions/closest-length-to-center (map solutions/parse-wire ["R75,D30,R83,U83,L12,D49,R71,U7,L72"
+                                                                          "U62,R66,U55,R34,D71,R55,D58,R83"]))))
 
-;;     (is (= 410
-;;            (solutions/closest-length-to-center (map solutions/parse-wire ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
-;;                                                                           "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]))))    
+    (is (= 410
+           (solutions/closest-length-to-center (map solutions/parse-wire ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+                                                                          "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]))))    
 
-;;     (is (= 15678
-;;            (solutions/closest-length-to-center (solutions/wires))))))
+    (is (= 15678
+           (solutions/closest-length-to-center (solutions/wires))))))
 
-;; (deftest day-4
-;;   (is (= 544
-;;          (count (solutions/passwords-part1 356261 846303))))
-;;   (is (= 334
-;;          (count (solutions/passwords-part2 356261 846303)))))
+(deftest day-4
+  (is (= 544
+         (count (solutions/passwords-part1 356261 846303))))
+  (is (= 334
+         (count (solutions/passwords-part2 356261 846303)))))
 
 
 (deftest day-5-parameter-modes
